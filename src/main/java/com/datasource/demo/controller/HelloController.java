ba6fa368d6d90f7e4c0ff74e2743156a06af209f
wangshuai <wangshuai@oa.bx>
2019-11-14 19:33:18 +0800

    test

--- src/main/java/com/datasource/demo/controller/HelloController.java
+++ src/main/java/com/datasource/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.datasource.demo.controller;
 import com.datasource.demo.service.HelloMainService;
 import com.datasource.demo.service.HelloOneService;
 import com.datasource.demo.service.HelloTwoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Wesley Wang
  * @date 2019/7/30 21:16
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class HelloController {
@@ -24,6 +26,9 @@ public class HelloController {
 
     @GetMapping("/t")
     public String getHello() {
+        log.debug("logging level debug");
+        log.info("logging level info");
+        log.error("logging level error");
         return "hello world";
     }
 
